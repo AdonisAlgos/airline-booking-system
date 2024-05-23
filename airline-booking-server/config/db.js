334608bd048ece9783d15bc8f77f924fe5dc95b5
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-05-23 15:32:21 +0100

    fixed bugs making server-side fully functionable

--- airline-booking-server/config/db.js
+++ airline-booking-server/config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
+
+dotenv.config({ path: "../.env" });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Failed to connect to MongoDB");
--- airline-booking-server/server.js
+++ airline-booking-server/server.js
@@ -15,5 +15,5 @@ connectDB();
 // Routes
 app.use("/aircraft", aircraftRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
