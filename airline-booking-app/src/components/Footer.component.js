02963d5e897396c9580b3ab29dcb71f3c8923a55
AdonisAlgos <adonisgeorgiou@hotmail.co.uk>
2024-05-24 14:01:29 +0100

    Updated footer

--- airline-booking-app/src/components/Footer.component.js
+++ airline-booking-app/src/components/Footer.component.js
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center col-12 py-3 my-4 border-top">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
           <span className="mb-3 mb-md-0 text-muted">
             © 2024 Aireal Company, Inc
